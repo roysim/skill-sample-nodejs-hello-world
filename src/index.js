f8463eaf254fc950745ff41f0e84534de396a054
Roy Simkhay <rsimkhay10@gsb.columbia.edu>
2017-03-17 13:59:23 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 var Alexa = require("alexa-sdk");
+var request = require('request');
+
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -15,6 +17,11 @@ var handlers = {
         this.emit('SayHello')
     },
     'SayHello': function () {
+        request('http://www.chapin.edu/data/calendar/rsscache/calendar_282.rss', function (error, response, body) {
+          console.log('error:', error); // Print the error if one occurred 
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+          console.log('body:', body); // Print the HTML for the Google homepage. 
+        });
         this.emit(':tell', 'Todays lunch is pizza.');
     }
 };
