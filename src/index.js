9b8b200935027838d8482c60c80a00a55e91c722
Roy Simkhay <rsimkhay10@gsb.columbia.edu>
2017-03-17 14:48:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ var handlers = {
     },
     'SayHello': function () {
         console.log('SayHello function start');
+        
+        var cal="";
 
         const request = require('request-promise')  
         const options = {  
@@ -23,19 +25,19 @@ var handlers = {
           uri: 'http://www.chapin.edu/data/calendar/rsscache/calendar_282.rss'
         }
         
-        request(options)  
+        cal = request(options)  
           .then(function (response) {
             console.log('request ok');
-            this.emit(response);
+            return(response);
           })
           .catch(function (err) {
             // Something bad happened, handle the error
             console.log('request ERROR');
             console.log(err);
-            this.emit(response);
+            return("");
             
           })
-        
+        console.log(cal);
         console.log('SayHello function end');
         this.emit(':tell', "I don't see any lunch on the calendar today.");
     }
