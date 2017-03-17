56d94c86a04c463bacf491113f666c0e6ec888d6
Roy Simkhay <rsimkhay10@gsb.columbia.edu>
2017-03-17 14:03:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,11 +17,12 @@ var handlers = {
         this.emit('SayHello')
     },
     'SayHello': function () {
-        request('http://www.chapin.edu/data/calendar/rsscache/calendar_282.rss', function (error, response, body) {
+        var result="";
+        request('http://www.chapin.edu/data/calendar/rsscache/calendar_282.rss', function (error, response, result) {
           console.log('error:', error); // Print the error if one occurred 
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-          console.log('body:', body); // Print the HTML for the Google homepage. 
+          console.log('body:', result); // Print the HTML for the Google homepage. 
         });
-        this.emit(':tell', 'Todays lunch is pizza.');
+        this.emit(':tell', result);
     }
 };
