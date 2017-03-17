3b089accea8750c3ede4aaf1cad524125f1b1f19
Roy Simkhay <rsimkhay10@gsb.columbia.edu>
2017-03-17 14:30:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict';
 var Alexa = require("alexa-sdk");
-var request = require('request');
-
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -18,23 +16,23 @@ var handlers = {
     },
     'SayHello': function () {
         console.log('SayHello function start');
-        request('http://www.chapin.edu/data/calendar/rsscache/calendar_282.rss', function (error, response, body) {
-            console.log('request start');
-            if (!error && response.statusCode == 200) {
-                console.log(body); // Show the HTML for the Modulus homepage.
-                this.emit(':tell', body);
-            }
-            else {
-                console.log(error);
-            }
-        });
+
+        const request = require('request-promise')  
+        const options = {  
+          method: 'GET',
+          uri: 'http://www.chapin.edu/data/calendar/rsscache/calendar_282.rss'
+        }
         
-        //Lets try to make a HTTP GET request to modulus.io's website.
-        request('http://www.modulus.io', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body); // Show the HTML for the Modulus homepage.
-            }
-        });
+        request(options)  
+          .then(function (response) {
+            console.log('request ok');
+          })
+          .catch(function (err) {
+            // Something bad happened, handle the error
+            console.log('request ERROR');
+            console.log(err);
+            
+          })
         
         console.log('SayHello function end');
         this.emit(':tell', "I don't see any lunch on the calendar today.");
