848b4746ee9b891a9033fa5af7637fb8957c2dd8
Roy Simkhay <rsimkhay10@gsb.columbia.edu>
2017-03-18 23:56:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ var handlers = {
     'SayLunch': function () {
         var dateSlot = this.event.request.intent.slots.Date
         if (dateSlot && dateSlot.value) {
-            var dateRequested = dateSlot.value;
+            var dateRequested = getDateFromSlot(dateSlot.value);
             console.log('Date specified.');
         }
         else {
@@ -49,3 +49,44 @@ var handlers = {
        
     } //saylunch
 }; //handlers
+
+// Given an AMAZON.DATE slot value parse out to usable JavaScript Date object
+// Utterances that map to the weekend for a specific week (such as �this weekend�) convert to a date indicating the week number and weekend: 2015-W49-WE.
+// Utterances that map to a month, but not a specific day (such as �next month�, or �December�) convert to a date with just the year and month: 2015-12.
+// Utterances that map to a year (such as �next year�) convert to a date containing just the year: 2016.
+// Utterances that map to a decade convert to a date indicating the decade: 201X.
+// Utterances that map to a season (such as �next winter�) convert to a date with the year and a season indicator: winter: WI, spring: SP, summer: SU, fall: FA)
+function getDateFromSlot(rawDate) {
+    // try to parse data
+    var date = new Date(Date.parse(rawDate));
+    var result;
+    // create an empty object to use later
+    var eventDate = {
+
+    };
+
+    // if could not parse data must be one of the other formats
+    if (isNaN(date)) {
+        // to find out what type of date this is, we can split it and count how many parts we have see comments above.
+        var res = rawDate.split("-");
+        // if we have 2 bits that include a 'W' week number
+        if (res.length === 2 && res[1].indexOf('W') > -1) {
+            var dates = getWeekData(res);
+            eventDate["startDate"] = new Date(dates.startDate);
+            eventDate["endDate"] = new Date(dates.endDate);
+            // if we have 3 bits, we could either have a valid date (which would have parsed already) or a weekend
+        } else if (res.length === 3) {
+            var dates = getWeekendData(res);
+            eventDate["startDate"] = new Date(dates.startDate);
+            eventDate["endDate"] = new Date(dates.endDate);
+            // anything else would be out of range for this skill
+        } else {
+            eventDate["error"] = dateOutOfRange;
+        }
+        // original slot value was parsed correctly
+    } else {
+        eventDate["startDate"] = new Date(date).setUTCHours(0, 0, 0, 0);
+        eventDate["endDate"] = new Date(date).setUTCHours(24, 0, 0, 0);
+    }
+    return eventDate;
+}
